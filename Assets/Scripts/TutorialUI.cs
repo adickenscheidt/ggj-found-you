d0f8ac695b99d0e0a40bc9d033641a9076681dd8
Laslo Scotton <laslo.scotton@gmx.de>
2021-01-29 00:05:35 +0100

    Tutorial Canvas
    Additional Time stop and continue

---
+++ Assets/Scripts/TutorialUI.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TutorialUI : MonoBehaviour
+{
+    [SerializeField]
+    GameObject tutorialUi;
+
+    private void Awake()
+    {
+        Cursor.visible = true;
+        Time.timeScale = 0;
+    }
+
+    public void HideTutorialCanvas()
+    {
+        tutorialUi.gameObject.SetActive(false);
+        Cursor.visible = false;
+        Time.timeScale = 1;
+    }
+}
