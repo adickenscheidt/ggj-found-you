2c859dd219a89801965a328f9dfc558fa23d413e
DustyJinx <jinkusu.yui@gmail.com>
2021-01-28 17:18:33 +0100

    [ADDED] PlayerMovement Script

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+    private int xAxis;
+    private int yAxis;
+    [SerializeField] private float moveSpeed;
+
+    private void Update() {
+        GetMovementInput();
+        MovePlayer();
+    }
+
+    private void GetMovementInput() {
+        xAxis = (int)Input.GetAxisRaw("Horizontal");
+        yAxis = (int)Input.GetAxisRaw("Vertical");
+    }
+
+    private void MovePlayer() {
+        Vector2 currentPosition = transform.position;
+        transform.position = new Vector2(currentPosition.x + xAxis * moveSpeed * Time.deltaTime, currentPosition.y + yAxis * moveSpeed * Time.deltaTime);
+    }
+}
