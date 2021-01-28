3129c357aadd0b64fb9ba4140d1dd58074fb839c
André Dickenscheidt <github@dickenscheidt.de>
2021-01-28 19:22:12 +0100

    Implemented basic fleeing AI

---
+++ Assets/VictimAI.cs
@@ -0,0 +1,59 @@
+using System;
+using UnityEngine;
+
+public enum VictimAIState
+{
+    Idle,
+    Fleeing,
+    RunningToHide,
+    Hidden,
+    Dead
+}
+
+public class VictimAI : MonoBehaviour
+{
+    public float sightRange;
+    public float movementSpeed;
+    [SerializeField] private VictimAIState currentState = VictimAIState.Fleeing;
+
+    private GameObject _player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _player = GameObject.Find("PlayerObject");
+    }
+
+    void FixedUpdate()
+    {
+        switch (currentState)
+        {
+            case VictimAIState.Idle:
+                if (IsPlayerInRange())
+                    currentState = VictimAIState.Fleeing;
+                break;
+            case VictimAIState.Fleeing:
+                if (!IsPlayerInRange())
+                    currentState = VictimAIState.Idle;
+                var awayFromPlayer = (transform.position - _player.transform.position).normalized;
+                transform.Translate(awayFromPlayer * (Time.fixedDeltaTime * movementSpeed));
+                break;
+            case VictimAIState.RunningToHide:
+                break;
+            case VictimAIState.Hidden:
+                break;
+            case VictimAIState.Dead:
+                break;
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawWireSphere(transform.position, sightRange);
+    }
+
+    private bool IsPlayerInRange()
+    {
+        return (_player.transform.position - transform.position).magnitude < sightRange;
+    }
+}
