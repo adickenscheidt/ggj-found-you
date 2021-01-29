d44a32422e8e091bc72536a1fffab47b9a2d7ded
Laslo Scotton <laslo.scotton@gmx.de>
2021-01-29 16:27:07 +0100

    Lighting and Camera
    Added Scripts and Lighting feature

--- Assets/Scripts/TutorialUI.cs
+++ Assets/Scripts/TutorialUI.cs
@@ -4,14 +4,35 @@ using UnityEngine;
 
 public class TutorialUI : MonoBehaviour
 {
+    //UI canvas
     [SerializeField]
     GameObject tutorialUi;
+
+    //Player Object
+    [SerializeField]
+    GameObject playerObject;
+
+    //Player Object SRenderer
+    SpriteRenderer spriteRenderer;
+
+    //The Spotlight that follows the player
+    [SerializeField]
+    Light playerLight;
 
     private void Awake()
     {
         tutorialUi.gameObject.SetActive(true);
         Cursor.visible = true;
         Time.timeScale = 0;
+        spriteRenderer = playerObject.GetComponent<SpriteRenderer>();
+
+        playerLight.range = 25;
+        playerLight.intensity = 14;
+    }
+
+    private void Start()
+    {
+        spriteRenderer.color = new Color(.3f, .3f, .3f, 1f);
     }
 
     public void HideTutorialCanvas()
@@ -19,5 +40,8 @@ public class TutorialUI : MonoBehaviour
         tutorialUi.gameObject.SetActive(false);
         Cursor.visible = false;
         Time.timeScale = 1;
+        playerLight.range = 35;
+        playerLight.intensity = 22;
+        spriteRenderer.color = new Color(.8f, .8f, .8f, 1f);
     }
 }
