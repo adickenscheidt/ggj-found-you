c548ec1957c40f2e6844dc17fe7fe53496746705
Laslo Scotton <laslo.scotton@gmx.de>
2021-01-29 21:55:27 +0100

    Added further Comments to scripts
    Not too important

--- Assets/Scripts/LightFollowPlayer.cs
+++ Assets/Scripts/LightFollowPlayer.cs
@@ -7,10 +7,12 @@ public class LightFollowPlayer : MonoBehaviour
     [SerializeField]
     Transform playerTransform;
 
+    //Offset vector of the Lamp towards the player
     Vector3 offset = new Vector3(0, 12, 0);
 
     private void Update()
     {
+        //makes the lamp follow the player
         transform.position = playerTransform.position + offset;
     }
 }
--- Assets/Scripts/TutorialUI.cs
+++ Assets/Scripts/TutorialUI.cs
@@ -21,11 +21,15 @@ public class TutorialUI : MonoBehaviour
 
     private void Awake()
     {
+        //sets the Ui visible
         tutorialUi.gameObject.SetActive(true);
+        //makes sure the cursor is visible
         Cursor.visible = true;
+        //sets time to 0
         Time.timeScale = 0;
         spriteRenderer = playerObject.GetComponent<SpriteRenderer>();
 
+        //dims the light
         playerLight.range = 25;
         playerLight.intensity = 14;
     }
@@ -35,13 +39,22 @@ public class TutorialUI : MonoBehaviour
         spriteRenderer.color = new Color(.3f, .3f, .3f, 1f);
     }
 
+
+    /// <summary>
+    /// Button Event, turns off ui canvas, activates time and Light.
+    /// </summary>
     public void HideTutorialCanvas()
     {
+        //deactivates the UI/Canvas
         tutorialUi.gameObject.SetActive(false);
+        //makes the cursor invisible
         Cursor.visible = false;
+        //the game time runs normally
         Time.timeScale = 1;
+        //creates a small cone of light around the player
         playerLight.range = 35;
         playerLight.intensity = 22;
+        //lightens up the players sprite (only 80%, as it is too bright normally)
         spriteRenderer.color = new Color(.8f, .8f, .8f, 1f);
     }
 }
