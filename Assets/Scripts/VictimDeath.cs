2708365fcee112d2f2f701f6819ca01ea2ac5141
Laslo Scotton <laslo.scotton@gmx.de>
2021-01-30 18:16:49 +0100

    Victim Death
    Blood splatter prefab and death sound missing.

--- Assets/Scripts/VictimDeath.cs
+++ Assets/Scripts/VictimDeath.cs
@@ -2,8 +2,34 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(AudioSource))]
 public class VictimDeath : MonoBehaviour
 {
     //Can be called from anywhere, destroys the player sprite and instantiates blood splatter on that spot (+sound)
 
+    //Bloodsplatter graphic
+    [SerializeField]
+    GameObject bloodSplatter;
+
+    //Sound
+    [SerializeField]
+    private AudioClip[] deathSoundsVictim;
+    private AudioSource audioSource;
+
+    private void Awake()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    public void VictimDies()
+    {
+        //Instantiates the bloodsplatter prefab
+        Instantiate(bloodSplatter, this.transform.position, Quaternion.identity);
+        //Play Death sound
+        audioSource.PlayOneShot(deathSoundsVictim[Random.Range(0, deathSoundsVictim.Length)]);
+        //Play death animation
+
+        //Disables Victim
+        this.gameObject.SetActive(false);
+    }
 }
